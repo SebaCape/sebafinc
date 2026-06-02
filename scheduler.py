4935c543fbca428ece77f5483878a1f6c51f92e5
SebaCape <scapellan55@gmail.com>
2026-06-02 00:50:37 -0400

    Fix datetime mismatch & venv bug in scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,16 +1,20 @@
 import schedule
 import time
+import sys
 import subprocess
 from datetime import datetime
+import zoneinfo
+
+#Ensures scheduler is ran from the virtual environment to prevent import errors
+PYTHON = sys.executable 
 
 #Check if current time is within US stock market hours (9:30am-4pm ET, Mon-Fri)
 def is_market_open():
-    now = datetime.now()
-    # Monday=0, Friday=4
+    now = datetime.now(zoneinfo.ZoneInfo("America/New_York"))
     if now.weekday() > 4:
         return False
-    market_open  = now.replace(hour=9, minute=30, second=0)
-    market_close = now.replace(hour=16, minute=0, second=0)
+    market_open  = now.replace(hour=9,  minute=30, second=0, microsecond=0)
+    market_close = now.replace(hour=16, minute=0,  second=0, microsecond=0)
     return market_open <= now <= market_close
 
 #Run the strategy script, which will fetch new data, update the database, and execute the backtest and paper trading logic
@@ -19,8 +23,8 @@ def run_strategy():
         print("Market closed — skipping")
         return
     print(f"Running strategy at {datetime.now()}")
-    subprocess.run(["python", "src/etl.py", "--auto"])
-    subprocess.run(["python", "src/main.py"])
+    subprocess.run([PYTHON, "src/etl.py", "--auto"])
+    subprocess.run([PYTHON, "src/main.py"])
 
 schedule.every(1).hours.do(run_strategy)
 
